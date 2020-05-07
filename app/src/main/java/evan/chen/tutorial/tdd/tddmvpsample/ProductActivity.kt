da01e6b701271a04ccd2352ebf170939913fad38
Evan <>
2020-05-07 16:46:34 +0800

    通過UI測試

--- app/src/main/java/evan/chen/tutorial/tdd/tddmvpsample/ProductActivity.kt
+++ app/src/main/java/evan/chen/tutorial/tdd/tddmvpsample/ProductActivity.kt
@@ -2,12 +2,28 @@ package evan.chen.tutorial.tdd.tddmvpsample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_product.*
+import java.text.NumberFormat
 
-class ProductActivity : AppCompatActivity(){
+class ProductActivity : AppCompatActivity(), ProductContract.IProductView {
+    private val productId = "pixel3"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product)
 
+        val productRepository = ProductRepository(ProductAPI())
+        val productPresenter = ProductPresenter(this, productRepository)
+        productPresenter.getProduct(productId)
+    }
+
+    override fun onGetResult(productResponse: ProductResponse) {
+        productName.text = productResponse.name
+        productDesc.text = productResponse.desc
+
+        val currencyFormat = NumberFormat.getCurrencyInstance()
+        currencyFormat.maximumFractionDigits = 0
+        val price = currencyFormat.format(productResponse.price)
+        productPrice.text = price
     }
 }
